8060c03af80cc6c527a931d884d3cae1f5b4b56e
Po0RiA <pooria.hassanzadeh@gmail.com>
2018-03-02 14:28:17 +0330

    udak secend setp
    remove logs , neccessry code

--- app/src/main/java/pooria/storeitems/EditorActivity.java
+++ app/src/main/java/pooria/storeitems/EditorActivity.java
@@ -24,11 +24,9 @@ import android.support.v4.content.Loader;
 
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -68,29 +66,24 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
   ImageView imageViewFrame;
   Uri uri;
   ItemsDbHelper dbHelper;
-  private Spinner mShopperSpinner;
   private Spinner mCategorySpinner;
   PackageManager packageManager;
   byte imageInByte[];
-  ArrayAdapter techShopperList;
-  ArrayAdapter clothesShopperList;
-  ArrayAdapter homeShopperList;
+
   ArrayAdapter categorySpinnerAdapter;
-private boolean isTouched=false;
 
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.edit_item);
-//make a instance of database
+    //make a instance of database
     dbHelper = new ItemsDbHelper(this);
-//initialize views like button Edittext and etc...
+    //initialize views like button Edittext and etc...
     nameBox = (EditText) findViewById(R.id.namebox);
     discriptionBox = (EditText) findViewById(R.id.discriptionbox);
     priceBox = (EditText) findViewById(R.id.pricebox);
     quantityBox = (EditText) findViewById(R.id.quantitybox);
-    mShopperSpinner = (Spinner) findViewById(R.id.spinner_items);
     mCategorySpinner = (Spinner) findViewById(R.id.category_list_item);
 
 
@@ -126,16 +119,6 @@ private boolean isTouched=false;
     mCategorySpinner.setAdapter(categorySpinnerAdapter);
 
 
-    techShopperList = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Tech_shoppers_List, android.R.layout.simple_spinner_item);
-    techShopperList.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-
-    clothesShopperList = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Clothes_shoppers_list, android.R.layout.simple_spinner_item);
-    clothesShopperList.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-
-    homeShopperList = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Home_shoppers_list, android.R.layout.simple_spinner_item);
-    homeShopperList.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 
 
     //active package manager for enable notification of permissions
@@ -176,77 +159,6 @@ private boolean isTouched=false;
     });
 
 
-    /*
-    mCategorySpinner.setOnTouchListener(new View.OnTouchListener() {
-                                      @Override
-                                      public boolean onTouch(View v, MotionEvent event) {
-                                       isTouched=true;
-                                        return false;
-                                      }
-                                    });
-
-    mCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-
-      if (isTouched){
-      final String selection = (String) parent.getItemAtPosition(position);
-
-      Log.i(LOG_TAG, "Category is :" + selection);
-
-      if (!TextUtils.isEmpty(selection)) {
-
-        if (selection.equals(getString(R.string.tech_List))) {
-          mCategoryNumber = 0;
-          mShopperSpinner.setAdapter(techShopperList);
-          Log.i(LOG_TAG, "we are in Tech");
-        }
-
-
-        if (selection.equals(getString(R.string.clothes_List))) {
-
-          mCategoryNumber = 1;
-          mShopperSpinner.setAdapter(clothesShopperList);
-
-          Log.i(LOG_TAG, "we are is Clothes");
-
-
-        }
-
-        if (selection.equals(getString(R.string.home_List))) {
-
-          mCategoryNumber = 2;
-          mShopperSpinner.setAdapter(homeShopperList);
-
-        }
-      }
-    }}
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-
-    }
-  });
-
-
-    mShopperSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-      @Override
-      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String selection = (String) parent.getItemAtPosition(position);
-        Log.i(LOG_TAG, "item selected is " + selection);
-        shopperCheck(selection);
-      }
-
-      @Override
-      public void onNothingSelected(AdapterView<?> parent) {
-
-      }
-    });
-
-*/
-
     uri = getIntent().getData();
 
     if (uri == null) {
@@ -254,123 +166,12 @@ private boolean isTouched=false;
 
     } else {
       setTitle("Update Item");
-      isTouched=true;
       getSupportLoaderManager().initLoader(0, null, this);
     }
 
 
   }
 
-  private void shopperCheck(String selectedShopper) {
-    Log.i(LOG_TAG, "we are in shoppering Check");
-
-    switch (mCategoryNumber) {
-
-      case 0:
-        Log.i(LOG_TAG, "we are at case 0 >> Tech");
-        checkTechShop(selectedShopper);
-        break;
-      case 1:
-        Log.i(LOG_TAG, "we are at case 1 >> Clothes");
-
-        checkClothesShop(selectedShopper);
-        break;
-      case 2:
-        Log.i(LOG_TAG, "we are at case 2 >> Home");
-
-        checkHomeShop(selectedShopper);
-        break;
-    }
-
-  }
-
-  private void checkTechShop(String selectedShopper) {
-    Log.i(LOG_TAG, "we are at checkTechShop");
-
-    if (selectedShopper.equals(getString(R.string.tech_shop1))) {
-      //if selectedShopper is shopper1 then mShopperNumber=1 !
-
-
-      mShopperNumber = 0;
-      Log.i(LOG_TAG, "Shopper Selected " + mShopperNumber);
-    }
-
-    if (selectedShopper.equals(getString(R.string.tech_shop2))) {
-      //if selectedShopper is shopper2 then mShopperNumber=2 !
-
-
-      mShopperNumber = 1;
-      Log.i(LOG_TAG, "Shopper Selected " + mShopperNumber);
-
-    } else if (selectedShopper.equals(getString(R.string.tech_shop3))) {
-      //if selectedShopper is shopper3 then mShopperNumber=3 !
-
-      mShopperNumber = 2;
-      Log.i(LOG_TAG, "Shopper Selected" + mShopperNumber);
-
-    } else if (selectedShopper.equals(getString(R.string.tech_shop4))) {
-      //if selectedShopper is shopper3 then mShopperNumber=3 !
-
-      mShopperNumber = 3;
-      Log.i(LOG_TAG, "Shopper Selected" + mShopperNumber);
-
-    } else if (selectedShopper.equals(getString(R.string.tech_shop5))) {
-      //if selectedShopper is shopper3 then mShopperNumber=3 !
-
-      mShopperNumber = 4;
-      Log.i(LOG_TAG, "Shopper Selected" + mShopperNumber);
-
-    }
-
-  }
-
-  private void checkClothesShop(String selectedShopper) {
-    Log.i(LOG_TAG, "we are at checkClothesShop");
-
-    if (selectedShopper.equals(getString(R.string.clothes_shop1))) {
-      //if selectedShopper is shopper1 then mShopperNumber=1 !
-
-
-      mShopperNumber = 0;
-      Log.i(LOG_TAG, "Shopper Selected " + mShopperNumber);
-    }
-
-    if (selectedShopper.equals(getString(R.string.clothes_shop2))) {
-      //if selectedShopper is shopper2 then mShopperNumber=2 !
-
-
-      mShopperNumber = 1;
-      Log.i(LOG_TAG, "Shopper Selected " + mShopperNumber);
-
-    } else if (selectedShopper.equals(getString(R.string.clothes_shop3))) {
-      //if selectedShopper is shopper3 then mShopperNumber=3 !
-
-      mShopperNumber = 2;
-      Log.i(LOG_TAG, "Shopper Selected" + mShopperNumber);
-
-    }
-
-  }
-
-  private void checkHomeShop(String selectedShopper) {
-
-    if (selectedShopper.equals(getString(R.string.Home_shop1))) {
-      mShopperNumber = 0;
-      Log.i(LOG_TAG, "shopper name  is :" + selectedShopper);
-    }
-    if (selectedShopper.equals(getString(R.string.Home_shop2))) {
-      mShopperNumber = 1;
-      Log.i(LOG_TAG, "shopper name  is :" + selectedShopper);
-
-    }
-    if (selectedShopper.equals(getString(R.string.Home_shop3))) {
-      mShopperNumber = 2;
-      Log.i(LOG_TAG, "shopper name  is :" + selectedShopper);
-
-    }
-
-
-  }
 
 
   @Override
@@ -516,23 +317,22 @@ private boolean isTouched=false;
     contentValue.put(ItemsContract.ItemsEntry.COLUMN_DESCRIPTION, discription);
     contentValue.put(ItemsContract.ItemsEntry.COLUMN_PRICE, price);
     contentValue.put(ItemsContract.ItemsEntry.COLUMN_QUANTITY, quantity);
-    //Log.i(LOG_TAG, "shopper is going to save " + mShopperNumber);
-    //contentValue.put(ItemsContract.ItemsEntry.COLUMN_SUPPLIER, mShopperNumber);
+
     contentValue.put(ItemsContract.ItemsEntry.COLUMN_CATEGORY, mCategoryNumber);
 
     contentValue.put(ItemsContract.ItemsEntry.COLUMN_IMAGE, imageInByte);
 
     //if uri is null means we have new item and must to add into database
     if (uri == null) {
-//insert item into database
+      //insert item into database
       Uri uri = getContentResolver().insert(ItemsContract.ItemsEntry.CONTENT_URI, contentValue);
 
       Log.i(LOG_TAG, "" + uri);
       Toast.makeText(this, "Your ITem Saved", Toast.LENGTH_SHORT).show();
     }
-//uri exist and we should to update this pat
+      //uri exist and we should to update this pat
     else {
-//do update method and getback number of row that updated
+      //do update method and getback number of row that updated
       int rowUpdated = getContentResolver().update(uri, contentValue, null, null);
       //if row update successful Toast a massage
       if (rowUpdated > 0) {
@@ -547,47 +347,6 @@ private boolean isTouched=false;
     }
   }
 
-  //insert method
- /* private void insert() {
-    //get access for writing on database
-    SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
-//get string or int data from input views
-    String name = nameBox.getText().toString().trim();
-
-
-    Log.e("name", "is:" + nameBox.getText().toString().trim());
-    String discription = discriptionBox.getText().toString().trim();
-
-    Log.e("name", "is:" + discriptionBox.getText().toString().trim());
-    Log.e("name", "is:" + discription.isEmpty());
-
-    int price = Integer.parseInt(priceBox.getText().toString().trim());
-    Log.e("name", "is:" + priceBox.getText().toString().trim());
-
-    int quantity = Integer.parseInt(quantityBox.getText().toString().trim());
-    Log.e("name", "is:" + quantityBox.getText().toString().trim());
-
-    //create new content value for add these variable for content
-    ContentValues contentValues = new ContentValues();
-
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_NAME, name);
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_DESCRIPTION, discription);
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_PRICE, price);
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_QUANTITY, quantity);
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_SUPPLIER, mShopperNumber);
-
-    if (TextUtils.isEmpty(name) || TextUtils.isEmpty(String.valueOf(price)) || TextUtils.isEmpty(String.valueOf(quantity))) {
-
-      Toast.makeText(this, "Please Complete all inputs", Toast.LENGTH_SHORT).show();
-
-    } else {
-
-      //do insert method on database
-      Long rowUpdated = sqLiteDatabase.insert(ItemsContract.ItemsEntry.TABLE_NAME_ITEMS, null, contentValues);
-      Toast.makeText(this, "Updated: " + rowUpdated, Toast.LENGTH_SHORT).show();
-    }
-  }
-*/
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu options from the res/menu/edit_menu.xml file.
@@ -658,7 +417,6 @@ private boolean isTouched=false;
         Log.e(LOG_TAG, "" + data.getPosition());
         // Extract out the value from the Cursor for the given column index
 
-        int id = data.getInt(idColumnIndex);
         String name = data.getString(nameIndex);
         String discription = data.getString(discriptionIndex);
         int price = data.getInt(priceIndex);
@@ -681,15 +439,8 @@ private boolean isTouched=false;
 
 
         mCategoryNumber = category;
-        //mShopperNumber = supplier;
 
-       // mCategorySpinner.setAdapter(categorySpinnerAdapter);
         mCategorySpinner.setSelection(mCategoryNumber);
-
-//mShopperSpinner.setAdapter(techShopperList);
-//Log.i(LOG_TAG,"LoadData0: "+supplier);
-//Log.i(LOG_TAG,"LoadData1: "+mShopperNumber);
-     //   mShopperSpinner.setSelection(3);
 
         // Update the views on the screen with the values from the database
 
@@ -698,8 +449,6 @@ private boolean isTouched=false;
         priceBox.setText(String.valueOf(price));
         quantityBox.setText(String.valueOf(quantity));
 
-        //mCategorySpinner.setSelection(0);
-        //mShopperSpinner.setSelection(supplier);
 
 
       }
@@ -720,102 +469,5 @@ private boolean isTouched=false;
 
   }
 
-/*
-private void testList(){
-  ArrayAdapter shopperAdapter = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Tech_shoppers_List, android.R.layout.simple_spinner_item);
-  shopperAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-  mShopperSpinner.setAdapter(shopperAdapter);
-
-  mShopperSpinner.setSelection(mShopperNumber);
-
-  mCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-      final String selection = (String) parent.getItemAtPosition(position);
-
-      Log.i(LOG_TAG, "Category is :" + selection);
-
-      if (!TextUtils.isEmpty(selection)) {
-
-        if (selection.equals(getString(R.string.tech_List))) {
-          mCategoryNumber = 0;
-
-          ArrayAdapter techShopperList = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Tech_shoppers_List, android.R.layout.simple_spinner_item);
-          techShopperList.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-          mShopperSpinner.setAdapter(techShopperList);
-
-
-          Log.i(LOG_TAG, "we are in Tech");
-
-        }
-
-
-        if (selection.equals(getString(R.string.clothes_List))) {
-          /**
-           * @param2 array list that we want to use for mShopperSpinner
-           * @param3 use a simple list vertical for item without any effect for showing better style
-           */
-
-
-   /*
-
-          ArrayAdapter clothesShopperList = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Clothes_shoppers_list, android.R.layout.simple_spinner_item);
-          // Specify dropdown layout style - simple list view with 1 item per line
-          clothesShopperList.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-          mShopperSpinner.setAdapter(clothesShopperList);
-          mCategoryNumber = 1;
-          Log.i(LOG_TAG, "we are is Clothes");
-
-
-        }
-        if (selection.equals(getString(R.string.home_List))) {
-
-
-          ArrayAdapter homeShopperList = ArrayAdapter.createFromResource(EditorActivity.this, R.array.Clothes_shoppers_list, android.R.layout.simple_spinner_item);
-
-
-          // Specify dropdown layout style - simple list view with 1 item per line
-          homeShopperList.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-          mShopperSpinner.setAdapter(homeShopperList);
-
-          mCategoryNumber = 2;
-
-        }
-      }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-
-    }
-  });
-  mShopperSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-      String selection = (String) parent.getItemAtPosition(position);
-      Log.i(LOG_TAG, "item selected is " + selection);
-      shopperCheck(selection);
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-  });
-
-
-}
-
-
-
-
-
-
-*/
 }
 
--- app/src/main/java/pooria/storeitems/MainActivity.java
+++ app/src/main/java/pooria/storeitems/MainActivity.java
@@ -27,10 +27,11 @@ import pooria.storeitems.data.ItemsContract;
 import pooria.storeitems.data.ItemsDbHelper;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-  private final static String LOG_TAG=MainActivity.class.getName();
-TextView textView;
+  private final static String LOG_TAG = MainActivity.class.getName();
+  TextView textView;
   ItemsDbHelper itemsDbHelper;
   CursorAdapter itemsAdapter;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -42,7 +43,7 @@ TextView textView;
 
 //set empty view when user doesnt select any item in list
     ListView listView = (ListView) findViewById(R.id.items_list_view);
-listView.setEmptyView(findViewById(R.id.emptyView));
+    listView.setEmptyView(findViewById(R.id.emptyView));
     itemsAdapter = new ItemsAdapter(this, null);
     listView.setAdapter(itemsAdapter);
     // displayDatabaseInfo();
@@ -59,162 +60,45 @@ listView.setEmptyView(findViewById(R.id.emptyView));
     });
 
 
-      listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-          Log.i(LOG_TAG, "id is :" + id);
-          Log.i(LOG_TAG, "id is :" + position);
-          Intent intent = new Intent(MainActivity.this, EditorActivity.class);
-          Uri uri = ContentUris.withAppendedId(ItemsContract.ItemsEntry.CONTENT_URI, id);
-          intent.setData(uri);
-          startActivity(intent);
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(LOG_TAG, "id is :" + id);
+        Log.i(LOG_TAG, "id is :" + position);
+        Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+        Uri uri = ContentUris.withAppendedId(ItemsContract.ItemsEntry.CONTENT_URI, id);
+        intent.setData(uri);
+        startActivity(intent);
 
-        }
-      });
+      }
+    });
 
   }
 
 
 
-
-
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-   // InserPet();
-    //displayDatabaseInfo();
-//insert();
-   // displayDatabaseInfo();
-  }
 
   private void InserPet() {
-    ContentValues contentValues=new ContentValues();
+    ContentValues contentValues = new ContentValues();
 
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_NAME,"ZenBook");
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_DESCRIPTION,"Is Very Nice Book");
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_PRICE,500);
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_QUANTITY,52);
-    contentValues.put(ItemsContract.ItemsEntry.COLUMN_SUPPLIER,1);
+    contentValues.put(ItemsContract.ItemsEntry.COLUMN_NAME, "ZenBook");
+    contentValues.put(ItemsContract.ItemsEntry.COLUMN_DESCRIPTION, "Is Very Nice Book");
+    contentValues.put(ItemsContract.ItemsEntry.COLUMN_PRICE, 500);
+    contentValues.put(ItemsContract.ItemsEntry.COLUMN_QUANTITY, 52);
+    contentValues.put(ItemsContract.ItemsEntry.COLUMN_SUPPLIER, 1);
 
 
-
-    Uri uri=getContentResolver().insert(ItemsContract.ItemsEntry.CONTENT_URI,contentValues);
-
+    Uri uri = getContentResolver().insert(ItemsContract.ItemsEntry.CONTENT_URI, contentValues);
 
 
   }
-
-  public void displayDatabaseInfo(){
-  //get access for write on database
-
-  SQLiteDatabase sqLiteDatabase=itemsDbHelper.getReadableDatabase();
-
-  //all we want to get back from cursor like name discription id and else ....
-String[] projection={ItemsContract.ItemsEntry.ID,
-  ItemsContract.ItemsEntry.COLUMN_NAME,
-  ItemsContract.ItemsEntry.COLUMN_DESCRIPTION,
-  ItemsContract.ItemsEntry.COLUMN_PRICE,
-  ItemsContract.ItemsEntry.COLUMN_QUANTITY,
-  ItemsContract.ItemsEntry.COLUMN_SUPPLIER};
-
-
-
-  /**
-   * this method is like this Select (projection=*) FROM TABLE_NAME_ITEMS
-   * {@link cursor} give us a random read-write access to result of this query with cursor we can change location throughout our table
-   */
- Cursor cursor = sqLiteDatabase.query(ItemsContract.ItemsEntry.TABLE_NAME_ITEMS, projection, null, null, null, null, null);
-
-  try {
-
-//we set schema of table to help us how we should insert items value for table
-
-textView.setText("number of rows is "+cursor.getCount() +"\n");
-    textView.append(ItemsContract.ItemsEntry.COLUMN_NAME+"-"
-      + ItemsContract.ItemsEntry.COLUMN_DESCRIPTION+"-"
-      + ItemsContract.ItemsEntry.COLUMN_PRICE+"-"
-      + ItemsContract.ItemsEntry.COLUMN_QUANTITY+"-"
-      + ItemsContract.ItemsEntry.COLUMN_SUPPLIER+"\n");
-
-
-
-    //get number of each column for getting inner value of that number
-    int idIndex=cursor.getColumnIndex(ItemsContract.ItemsEntry.ID);
-    int nameIndex=cursor.getColumnIndex(ItemsContract.ItemsEntry.COLUMN_NAME);
-    int discriptionIndex=cursor.getColumnIndex(ItemsContract.ItemsEntry.COLUMN_DESCRIPTION);
-    int priceIndex=cursor.getColumnIndex(ItemsContract.ItemsEntry.COLUMN_PRICE);
-    int quantityIndex=cursor.getColumnIndex(ItemsContract.ItemsEntry.COLUMN_QUANTITY);
-    int supplierIndex=cursor.getColumnIndex(ItemsContract.ItemsEntry.COLUMN_SUPPLIER);
-
-//until next cursor is exist that mean (until we have another row to count) cursor must goes to that.
-    while (cursor.moveToNext()){
-//for each row add every column index to string
-      int id=cursor.getInt(idIndex);
-      String name=cursor.getString(nameIndex);
-      String discription=cursor.getString(discriptionIndex);
-      int price=cursor.getInt(priceIndex);
-      int quantity=cursor.getInt(quantityIndex);
-      int supplier=cursor.getInt(supplierIndex);
-      String s="";
-      if (supplier== ItemsContract.ItemsEntry.shopper1){
-        s=getString(R.string.tech_List);
-      }if (supplier== ItemsContract.ItemsEntry.shopper2){
-        s=getString(R.string.clothes_List);
-
-      }else if (supplier== ItemsContract.ItemsEntry.shopper3){
-        s=getString(R.string.home_List);
-      }
-
-
-
-
-//add all string to textView
-      textView.append(id+"-"+name+"-"+discription+"-"+price+"-"+quantity+"-"+s+"\n");
-
-
-    }
-
-
-
-  }finally {
-    //when cursor finish close it to release memory
-    cursor.close();
-  }
-
-
-
-}
-
-
-//write new item into database
-private void insert(){
-//get access for writing on database
-  SQLiteDatabase sqLiteDatabase=itemsDbHelper.getWritableDatabase();
-
-  //for add new item to listItems we use content value with 2 params
-  /*
-   *  contentValues.put(ColumnKey,columnValue);
-   */
-  ContentValues contentValues=new ContentValues();
-  contentValues.put(ItemsContract.ItemsEntry.COLUMN_NAME,"ZenBook");
-  contentValues.put(ItemsContract.ItemsEntry.COLUMN_DESCRIPTION,"Is Very Nice Book");
-  contentValues.put(ItemsContract.ItemsEntry.COLUMN_PRICE,500);
-  contentValues.put(ItemsContract.ItemsEntry.COLUMN_QUANTITY,52);
-  contentValues.put(ItemsContract.ItemsEntry.COLUMN_SUPPLIER,1);
-
-//send request for adding new row to database with tablename,null,and content
-  Long rowUpdated=sqLiteDatabase.insert(ItemsContract.ItemsEntry.TABLE_NAME_ITEMS,null,contentValues);
-
-  Toast.makeText(this, "Updated: "+rowUpdated, Toast.LENGTH_SHORT).show();
-}
 
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu options from the res/menu/menu_editor.xml file.
     // This adds menu items to the app bar.
-    getMenuInflater().inflate(R.menu.main_menu,menu);
+    getMenuInflater().inflate(R.menu.main_menu, menu);
     return super.onCreateOptionsMenu(menu);
   }
 
@@ -222,17 +106,17 @@ private void insert(){
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
 //check if menu item is delete button then delete list;
-    if (item.getItemId()==R.id.delete_button){
+    if (item.getItemId() == R.id.delete_button) {
       deleteList();
       //read again database to update list
 //displayDatabaseInfo();
     }
-    if (item.getItemId()==R.id.insert)
-    {
+    if (item.getItemId() == R.id.insert) {
       InserPet();
-    }if (item.getItemId()==R.id.basket){
+    }
+    if (item.getItemId() == R.id.basket) {
 
-      Intent intent=new Intent(this,ShopListActivity.class);
+      Intent intent = new Intent(this, ShopListActivity.class);
       startActivity(intent);
 
     }
@@ -241,11 +125,9 @@ private void insert(){
   }
 
 
-
-
   //delete list
-  private void deleteList(){
-int id=getContentResolver().delete(ItemsContract.ItemsEntry.CONTENT_URI,null,null);
+  private void deleteList() {
+    int id = getContentResolver().delete(ItemsContract.ItemsEntry.CONTENT_URI, null, null);
 
 
   }
@@ -253,18 +135,9 @@ int id=getContentResolver().delete(ItemsContract.ItemsEntry.CONTENT_URI,null,nul
 
   @Override
   public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-    /*
-    String[]projection=new String[]{ItemsContract.ItemsEntry.ID,
-      ItemsContract.ItemsEntry.COLUMN_NAME,
-      ItemsContract.ItemsEntry.COLUMN_DESCRIPTION,
-      ItemsContract.ItemsEntry.COLUMN_PRICE,
-      ItemsContract.ItemsEntry.COLUMN_QUANTITY,
-      ItemsContract.ItemsEntry.COLUMN_SUPPLIER,
-      ItemsContract.ItemsEntry.COLUMN_CATEGORY,
-      ItemsContract.ItemsEntry.COLUMN_IMAGE};
-    */
 
-    String[]projection=new String[]{ItemsContract.ItemsEntry.ID,
+
+    String[] projection = new String[]{ItemsContract.ItemsEntry.ID,
       ItemsContract.ItemsEntry.COLUMN_NAME,
       ItemsContract.ItemsEntry.COLUMN_DESCRIPTION,
       ItemsContract.ItemsEntry.COLUMN_PRICE,
@@ -273,23 +146,22 @@ int id=getContentResolver().delete(ItemsContract.ItemsEntry.CONTENT_URI,null,nul
       ItemsContract.ItemsEntry.COLUMN_IMAGE};
 
 
-    return new CursorLoader(this,ItemsContract.ItemsEntry.CONTENT_URI,projection,null,null,null);
+    return new CursorLoader(this, ItemsContract.ItemsEntry.CONTENT_URI, projection, null, null, null);
 
   }
 
   @Override
   public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-    Log.e(LOG_TAG,"LoaderOnLoad Finish");
+    Log.e(LOG_TAG, "LoaderOnLoad Finish");
     itemsAdapter.swapCursor(data);
 
-    //data.close();
-//itemsAdapter.swapCursor(null);
+
   }
 
   @Override
   public void onLoaderReset(Loader<Cursor> loader) {
-    Log.e(LOG_TAG,"LoaderOnLoad Reset");
-   itemsAdapter.swapCursor(null);
+    Log.e(LOG_TAG, "LoaderOnLoad Reset");
+    itemsAdapter.swapCursor(null);
 
   }
 }
